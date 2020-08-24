49e997fee66cf6348587dba15beaf053126ecde3
Shubhendu Goel <smartyshubhendu@gmail.com>
2020-08-24 22:09:10 +0530

    Added Home Page files

---
+++ application/views/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+	<html>
+	<head>
+		<title>Interview Bit</title>
+<meta name="viewport" content="width=device-width,initial-scale=1.0" charset="utf-8">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href=<?php base_url();?>"style.css" 	>
+<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Poppins">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script></head>
+<div id="flashdata"></div>
+<body>
